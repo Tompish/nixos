49601958d13864f2b20afa3cc71a1fac3cc78c9b
tompish <euler.linder@gmail.com>
2026-02-08 22:04:23 +0100

    Add hjkl keybinds to tmux

--- home/develop/develop.nix
+++ home/develop/develop.nix
@@ -146,6 +146,10 @@
 	  extraConfig = ''
 			  bind '"' split-window -v -c "#{pane_current_path}"
 			  bind '%' split-window -h -c "#{pane_current_path}"
+			  bind-key h select-pane -L
+			  bind-key j select-pane -D
+			  bind-key k select-pane -U
+			  bind-key l select-pane -R
 	  '';
   };
 
